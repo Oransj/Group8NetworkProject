e35a632874aea531318e1cc0908e3c39c220c9fe
Mark Kour <mark.kour@icloud.com>
2022-10-18 12:07:49 +0200

    Replaced current gradle files with the default

--- project/src/main/resources/static/js/index.js
+++ project/src/main/resources/static/js/index.js
@@ -1,23 +1,31 @@
-window.onload = function() {
-    insertWeatherCard("4", "Monday", "thunder", "6", "8", "4.0", "2.0");
+window.onload = function () {
+  insertWeatherCard("4", "Monday", "thunder", "6", "8", "4.0", "2.0");
 
-    addEventListeners();
-}
+  addEventListeners();
+};
 
 /* Setup weather cards with correct data */
 var weatherData = [];
 
 function getWeatherData() {
-    throw "Not implemented yet";
+  throw "Not implemented yet";
 }
 
 function insertWeatherCards() {
-    throw "Not implemented yet";
+  throw "Not implemented yet";
 }
 
-function insertWeatherCard(date, weekday, weatherType, minTemp, maxTemp, rainAmount, windAmount) {
-    let weatherCardContainer = document.querySelector(".weather-card-container");
-    weatherCardContainer.innerHTML += `
+function insertWeatherCard(
+  date,
+  weekday,
+  weatherType,
+  minTemp,
+  maxTemp,
+  rainAmount,
+  windAmount
+) {
+  let weatherCardContainer = document.querySelector(".weather-card-container");
+  weatherCardContainer.innerHTML += `
         <li class="weather-card" data-value="${date}">
             <h4 class="weather-card--date">${weekday}</h4>
             <img class="weather-card--icon" src="../img/animated/${weatherType}.svg" alt="Weather icon">
@@ -36,25 +44,27 @@ function insertWeatherCard(date, weekday, weatherType, minTemp, maxTemp, rainAmo
 }
 
 function addEventListeners() {
-    /* Add event listeners to all weather cards:
+  /* Add event listeners to all weather cards:
     Save date and switch to the weather report page */
 
-    let weatherCards = document.querySelectorAll(".weather-card");
-    weatherCards.forEach(card => card.addEventListener("click", function() {
-        switchToForecastPage(card)
-    }));
+  let weatherCards = document.querySelectorAll(".weather-card");
+  weatherCards.forEach((card) =>
+    card.addEventListener("click", function () {
+      switchToForecastPage(card);
+    })
+  );
 }
 
 function switchToForecastPage(element) {
-    saveDate(element);
-    setPage("weather_report.html");
+  saveDate(element);
+  setPage("../html/weather_report.html");
 }
 
 function saveDate(element) {
-    let data = element.getAttribute("data-value");
-    localStorage.setItem("clicked_date", data);
+  let data = element.getAttribute("data-value");
+  localStorage.setItem("clicked_date", data);
 }
 
 function setPage(url) {
-    window.location.href = url;
+  window.location.href = url;
 }
