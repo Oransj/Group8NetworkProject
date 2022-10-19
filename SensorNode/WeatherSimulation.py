943e5468d347509c80133cff48de812177de0612
Oransj <chrion@stud.ntnu.no>
2022-10-19 16:02:37 +0200

    Create day class

--- SensorNode/WeatherSimulation.py
+++ SensorNode/WeatherSimulation.py
@@ -23,3 +23,29 @@ class weather:
 
     def __str__(self) -> str:
         return f"Temperature: {self.temperature}°C, Precipitation: {self.precipitation}mm, Lux: {self.lux}lx, Pascal: {self.pascal}Pa, Windpower: {self.windpower}m/s, Winddir: {self.winddir}"
+
+class day:
+    def __init__(self) -> None:
+        """The constructor of the day class. The day class hold the weather data for each time slot of the day.
+        """                
+        self.weatherList = {}
+        i=0
+        minutes_to_add = 15
+        time = datetime.datetime(2020, 1, 1, 0, 0, 0)
+        for i in range(int(24*60/minutes_to_add)):
+            self.weatherList[time.time()] = None
+            time = time + datetime.timedelta(minutes=minutes_to_add)
+
+    def add_weather(self, time : datetime.time, weather : weather):
+        """Adds a weather object to the assigned time slot.
+
+        Args:
+            time (datetime.time): The time slot.
+            weather (weather): The weather object to assign to the time slot.
+        """        
+        self.weatherList[time] = weather
+        print(time)
+        print(f"{self.weatherList[time]} TIME: {time}")
+        
+newDay = day()
+newDay.add_weather(datetime.time(0,0,0), weather(10, 0.0, 0.0, 0, 0, 'N'))
