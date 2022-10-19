7dcf81d8f369af51290701af1d6a8530c8dbbe03
Oransj <chrion@stud.ntnu.no>
2022-10-19 16:02:05 +0200

    Create weather class

--- SensorNode/WeatherSimulation.py
+++ SensorNode/WeatherSimulation.py
@@ -1,1 +1,25 @@
+import datetime
 
+    
+class weather:
+
+    def __init__(self, temperature : int, precipitation : float, lux : float, pascal : int, windpower : int, winddir : chr) -> None:
+        """The constructor of the weather class. The weather class hold the weather data for the simulation and one unit of time.
+
+        Args:
+            temperature (int): The temperature in degrees celsius.
+            precipitation (float): The precipitation in millimeters.
+            lux (float): The lux value.
+            pascal (int): The pascal value.
+            windpower (int): The windpower in meters per second.
+            winddir (chr): The wind direction.
+        """        
+        self.temperature = temperature
+        self.precipitation = precipitation
+        self.lux = lux
+        self.pascal = pascal
+        self.windpower = windpower
+        self.winddir = winddir
+
+    def __str__(self) -> str:
+        return f"Temperature: {self.temperature}°C, Precipitation: {self.precipitation}mm, Lux: {self.lux}lx, Pascal: {self.pascal}Pa, Windpower: {self.windpower}m/s, Winddir: {self.winddir}"
