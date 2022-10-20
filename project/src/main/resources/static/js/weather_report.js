435e52da2a2cc429d7079e3f35f00ab0393f9d77
z-rayc <zilerayser@gmail.com>
2022-10-20 23:20:01 +0200

    Add placeholder temperature graph

--- project/src/main/resources/static/js/weather_report.js
+++ project/src/main/resources/static/js/weather_report.js
@@ -40,29 +40,53 @@ fetch('https://data.cdc.gov/resource/w9j2-ggv5.csv')
   });
 
 function csvToSeries(text) {
-  const lifeExp = 'average_life_expectancy'
-  // Convert the data text (CSV) to Jason
+  // Convert the data text (CSV) to JSON
+  /*
+  let tempAsString = '{"time": ["00:00", "01:00", "02:00", "03:00"],"temp": [7.0, 8.0, 7.6, 8.0]}';
+  let tempAsJson = JSON.parse(tempAsString);
   let dataAsJson = JSC.csv2Json(text);
-  let male = [], female = [];
-  dataAsJson.forEach(function (row) {
-    if (row.race === 'All Races') {
-      if (row.sex === 'Male') {
-        // Add data to male array
-        male.push({ x: row.year, y: row[lifeExp] });
-      } else if (row.sex === 'Female') {
-        // Add data to female array
-        female.push({ x: row.year, y: row[lifeExp] });
-      }
-    }
-  });
+  */
+  let temp = [
+    {x: "00:00", y: 7.0},
+    {x: "01:00", y: 8.0},
+    {x: "02:00", y: 7.6},
+    {x: "03:00", y: 8.2},
+    {x: "04:00", y: 8.3},
+    {x: "05:00", y: 8.6},
+    {x: "06:00", y: 8.7},
+    {x: "07:00", y: 10.1},
+    {x: "08:00", y: 12.3},
+    {x: "09:00", y: 12.6},
+    {x: "10:00", y: 12.9},
+    {x: "11:00", y: 13.0},
+    {x: "12:00", y: 13.6},
+    {x: "13:00", y: 13.8},
+    {x: "14:00", y: 13.7},
+    {x: "15:00", y: 13.6},
+    {x: "16:00", y: 13.7},
+    {x: "17:00", y: 13.5}
+  ];
+
+  /* tempAsJson.forEach(function (row) {
+    temp.push({x: row.time, y: row.temp})
+  }); */
+
   return [
-    { name: 'Male', points: male },
-    { name: 'Female', points: female }
+    { name: 'Temperature', points: temp },
   ];
 }
 
 function renderChart(series) {
-  JSC.Chart(forecastGraphTemp, {
+  return JSC.Chart(forecastGraphTemp, {
+    type: 'Line',
+    elements_point_radius: 0,
+    title_label_text: 'Temperature',
+    titleBox_position: 'center',
+    xAxis_label_text: 'Time',
+    yAxis_label_text: 'Celsius',
+    legend_visible: false,
+    defaultPoint_tooltip: '%xValue &nbsp;&nbsp;<b>%yValue</b> degrees',
+    defaultPoint_marker_visible: false,
     series: series
   });
 }
