7fc291316c9110f95bb522889f17a467f9133be9
z-rayc <zilerayser@gmail.com>
2022-10-20 11:50:30 +0200

    Remove useless event listener and add comments

--- project/src/main/resources/static/js/main.js
+++ project/src/main/resources/static/js/main.js
@@ -10,16 +10,14 @@ function getLastUpdatedTime() {
 
 var date = new Date();
 var finaltime = timeSince(date);
+lastUpdatedElem.innerHTML = "This page was updated " + timeSince(date) + " ago";
 
-setLastUpdated.onclick = function() {
-    setInterval();
-}
-
+/* Set an interval for the text to be refreshed on the page */
 setInterval(function() {
   lastUpdatedElem.innerHTML = "This page was updated " + timeSince(date) + " ago";
 }, 30000);
-lastUpdatedElem.innerHTML = "This page was updated " + timeSince(date) + " ago";
 
+/* Get the amount of time since the page was updated */
 function timeSince(date) {
   var seconds = Math.floor((new Date() - date) / 1000);
   var interval = Math.floor(seconds / 31536000);
