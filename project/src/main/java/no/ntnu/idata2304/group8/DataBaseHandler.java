1879ed2d52ed65254e8f77be77236c4318bc5905
Kim <71408597+Kim3n@users.noreply.github.com>
2022-10-20 10:58:26 +0200

    First commit

---
+++ project/src/main/java/no/ntnu/idata2304/group8/DataBaseHandler.java
@@ -0,0 +1,42 @@
+package no.ntnu.idata2304.group8;
+
+public class DataBaseHandler {
+  SQLHandler SQLHandler = new SQLHandler();
+  public void writeToDB(){
+    SQLHandler.insert(12345, "testing");
+    //TODO: Implement method
+  }
+
+  public void readFromDB(){
+    //TODO: Implement method
+  }
+  public void updateData(){
+    //TODO: Implement method
+  }
+
+  /**
+   * Splits the incoming string into separate numbers and returns an int[]
+   * @param incomingString - String of numbers from sensorNode
+   * @return Int[]
+   */
+  public static int[] convertStringToArray(String incomingString){
+    int[] intArray = new int[5];
+    String[] parts = incomingString.split("(?<=\\G..)");
+    for (int i = 0; i < parts.length; i++){
+      intArray[i] = Integer.parseInt(parts[i]);
+    }
+    return intArray;
+  }
+
+
+  public static void main(String[] args) {
+    int[] a = convertStringToArray("112233113");
+    for (int i = 0; i< a.length; i++){
+      System.out.println(a[i]);
+      DataBaseHandler dataBaseHandler = new DataBaseHandler();
+      dataBaseHandler.writeToDB();
+    }
+  }
+
+
+}
