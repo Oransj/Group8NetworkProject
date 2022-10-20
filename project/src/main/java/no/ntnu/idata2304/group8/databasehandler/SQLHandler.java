3723236589a2aef0f47cb5a15bd0c681d368e5a4
Kim <71408597+Kim3n@users.noreply.github.com>
2022-10-20 21:58:19 +0200

    Implemented addData()

--- project/src/main/java/no/ntnu/idata2304/group8/databasehandler/SQLHandler.java
+++ project/src/main/java/no/ntnu/idata2304/group8/databasehandler/SQLHandler.java
@@ -1,8 +1,16 @@
 package no.ntnu.idata2304.group8.databasehandler;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
+
+
 
 public class SQLHandler {
         /**
@@ -12,7 +20,8 @@ public class SQLHandler {
 
     private Connection connect() {
         // Location of SQLlite database
-        String url = "jdbc:sqlite:C:/Users/Kim Andre/OneDrive - NTNU/gitHub/Group8NetworkProject/project/src/main/test.db";
+        String fileName = "src/main/resources/database/test.db";
+        String url = "jdbc:sqlite:" + fileName;
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(url);
@@ -21,6 +30,42 @@ public class SQLHandler {
         }
         return connection;
     }
+
+    public void addData() throws IOException, ParseException {
+        String fileName = "src/main/resources/database/test.json";
+        Object obj = new JSONParser().parse(new FileReader(fileName));
+        JSONObject jo = (JSONObject) obj;
+        long millis = (long) jo.get("Millis");
+        double temperature = (double) jo.get("Temperature");
+        double precipitation = (double) jo.get("Precipitation");
+        double air_pressure = (double) jo.get("Air_pressure");
+        double light = (double) jo.get("Light");
+        long wind_speed = (long) jo.get("Wind_Speed");
+        double wind_dir = (double) jo.get("Wind_Dir");
+
+        String sql = "INSERT INTO Weather(Time, Temprature, Precipitation, Air_pressure, Light, Wind_Speed, Wind_dir) VALUES(?,?,?,?,?,?,?)";
+        try (Connection connection = this.connect();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setLong(1, millis);
+            pstmt.setDouble(2, temperature);
+            pstmt.setDouble(3,precipitation);
+            pstmt.setDouble(4,air_pressure);
+            pstmt.setDouble(5,light);
+            pstmt.setDouble(6,wind_speed);
+            pstmt.setDouble(7,wind_dir);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+
+
+    }
+
+
+
 
     /**
      * Inserts data into the Database
@@ -62,19 +107,44 @@ public class SQLHandler {
 
                 // loop through the result set
                 while (rs.next()) {
-                    array.add(rs.getInt("Time"));
-                    array.add(rs.getFloat("Temprature"));
-                    array.add(rs.getFloat("Precipitation"));
-                    array.add(rs.getInt("Air_pressure"));
-                    array.add(rs.getFloat("Light"));
-                    array.add(rs.getFloat("Wind_Speed"));
-                    array.add((rs.getFloat("Wind_dir")));
+                    array.add(rs.getLong("Time"));
+                    array.add(rs.getDouble("Temprature"));
+                    array.add(rs.getDouble("Precipitation"));
+                    array.add(rs.getDouble("Air_pressure"));
+                    array.add(rs.getDouble("Light"));
+                    array.add(rs.getDouble("Wind_Speed"));
+                    array.add((rs.getDouble("Wind_dir")));
                 }
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
             return array;
         }
+
+    public List getAllAtTime(int Time){
+        String sql = "SELECT Temprature, Precipitation, Air_pressure, Light, Wind_Speed, Wind_dir FROM Weather WHERE Time = ?;";
+
+        ArrayList array = new ArrayList<>();
+
+        try (Connection connection = this.connect();
+             PreparedStatement pstmt  = connection.prepareStatement(sql)) {
+            pstmt.setInt(1,Time);
+            ResultSet rs  = pstmt.executeQuery();
+
+            // loop through the result set
+            while (rs.next()) {
+                array.add(rs.getFloat("Temprature"));
+                array.add(rs.getFloat("Precipitation"));
+                array.add(rs.getInt("Air_pressure"));
+                array.add(rs.getFloat("Light"));
+                array.add(rs.getFloat("Wind_Speed"));
+                array.add((rs.getFloat("Wind_dir")));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return array;
+    }
 
     /**
      * Queries the database to find Temprature at a certain time
@@ -243,14 +313,14 @@ public class SQLHandler {
          */
         //TESTING
 
-        public static void main(String[] args) {
+        public static void main(String[] args) throws IOException, ParseException {
         SQLHandler app = new SQLHandler();
-        ArrayList testArray = new ArrayList();
-
-        testArray = (ArrayList) app.getAir_pressure(12);
-
-        for (int i=0; i < testArray.size(); i++){
-            System.out.println(testArray.get(i));
+            List s = app.selectAll();
+            for(int i=0; i< s.size();i++){
+                if(i%7 == 0){
+                    System.out.println("");
+                }
+                System.out.println(s.get(i));
         }
         }
 }
