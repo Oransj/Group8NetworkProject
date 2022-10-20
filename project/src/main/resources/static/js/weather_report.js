912fe7fc878b7941ef71f8b3d79a869b1c139d5a
z-rayc <zilerayser@gmail.com>
2022-10-20 23:19:19 +0200

    Remove loadData function used for testing

--- project/src/main/resources/static/js/weather_report.js
+++ project/src/main/resources/static/js/weather_report.js
@@ -1,18 +1,9 @@
 window.onload = function () {
-  loadData();
-  console.log(data);
-
   insertForecastTableRow("00:00", "moon", "2", "0.4", "1.5", 90);
   insertForecastTableRow("01:00", "moon", "2", "0.4", "2.0", 85);
   insertForecastTableRow("02:00", "moon", "2", "0.3", "1.8", 70);
   insertForecastTableRow("03:00", "moon", "3", "0.3", "2.4", 60);
   insertForecastTableRow("03:00", "cloud-with-moon", "4", "0.2", "2.0", 60);
-}
-
-var data = 0;
-
-function loadData() {
-  data = localStorage.getItem("clicked_date");
 }
 
 var forecastTableBody = document.querySelector(".forecast_table--body");
