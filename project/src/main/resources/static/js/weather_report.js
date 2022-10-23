a591d756130e632b7414fd8d9f12894c71cae80c
z-rayc <zilerayser@gmail.com>
2022-10-23 14:37:47 +0200

    Remove unnecessary fetch and commented out code

--- project/src/main/resources/static/js/weather_report.js
+++ project/src/main/resources/static/js/weather_report.js
@@ -43,55 +43,31 @@ function insertForecastTableRow(time, weatherType, temp, rainAmount, windAmount,
   `;
 }
 
-
-fetch('https://data.cdc.gov/resource/w9j2-ggv5.csv')
-  .then(function (response) {
-    return response.text();
-  })
-  .then(function (text) {
-    let series = csvToSeries(text);
-    renderChart(series);
-  })
-  .catch(function (error) {
-    // Something went wrong
-    console.log(error);
-  });
-
-function csvToSeries(text) {
-  // Convert the data text (CSV) to JSON
-  /*
-  let tempAsString = '{"time": ["00:00", "01:00", "02:00", "03:00"],"temp": [7.0, 8.0, 7.6, 8.0]}';
-  let tempAsJson = JSON.parse(tempAsString);
-  let dataAsJson = JSC.csv2Json(text);
-  */
+function tempToSeries(text) {
   let temp = [
-    {x: "00:00", y: 7.0},
-    {x: "01:00", y: 8.0},
-    {x: "02:00", y: 7.6},
-    {x: "03:00", y: 8.2},
-    {x: "04:00", y: 8.3},
-    {x: "05:00", y: 8.6},
-    {x: "06:00", y: 8.7},
-    {x: "07:00", y: 10.1},
-    {x: "08:00", y: 12.3},
-    {x: "09:00", y: 12.6},
-    {x: "10:00", y: 12.9},
-    {x: "11:00", y: 13.0},
-    {x: "12:00", y: 13.6},
-    {x: "13:00", y: 13.8},
-    {x: "14:00", y: 13.7},
-    {x: "15:00", y: 13.6},
-    {x: "16:00", y: 13.7},
-    {x: "17:00", y: 13.5}
+    { x: "00:00", y: 7.0 },
+    { x: "01:00", y: 8.0 },
+    { x: "02:00", y: 7.6 },
+    { x: "03:00", y: 8.2 },
+    { x: "04:00", y: 8.3 },
+    { x: "05:00", y: 8.6 },
+    { x: "06:00", y: 8.7 },
+    { x: "07:00", y: 10.1 },
+    { x: "08:00", y: 12.3 },
+    { x: "09:00", y: 12.6 },
+    { x: "10:00", y: 12.9 },
+    { x: "11:00", y: 13.0 },
+    { x: "12:00", y: 13.6 },
+    { x: "13:00", y: 13.8 },
+    { x: "14:00", y: 13.7 },
+    { x: "15:00", y: 13.6 },
+    { x: "16:00", y: 13.7 },
+    { x: "17:00", y: 13.5 }
   ];
 
-  /* tempAsJson.forEach(function (row) {
-    temp.push({x: row.time, y: row.temp})
-  }); */
-
-  return [
-    { name: 'Temperature', points: temp },
-  ];
+  return [{
+    name: 'Temperature', points: temp
+  }];
 }
 
 const forecastGraphTemp = document.querySelector(".forecast_graph--temperature");
