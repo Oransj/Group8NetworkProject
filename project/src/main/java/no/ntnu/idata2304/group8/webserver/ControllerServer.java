213282e97caa9c70acafeeb28b99951e7620c103
Mark Kour <mark.kour@icloud.com>
2022-10-27 13:40:17 +0200

    Created getReports method

--- project/src/main/java/no/ntnu/idata2304/group8/webserver/ControllerServer.java
+++ project/src/main/java/no/ntnu/idata2304/group8/webserver/ControllerServer.java
@@ -1,9 +1,11 @@
 package no.ntnu.idata2304.group8.webserver;
 
 
+import com.google.gson.Gson;
 import no.ntnu.idata2304.group8.databasehandler.SQLHandler;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 @RestController
@@ -13,5 +15,16 @@ public class ControllerServer {
   @PostMapping(value = "/api/front")
   public void processJSON(@RequestBody Map<String, Object> payload) {
     System.out.println(payload);
+  }
+
+  @GetMapping("/api/front")
+  public String getReports() {
+    SQLHandler sqlHandler = new SQLHandler();
+//    ArrayList<String> list = sqlHandler.selectAll();
+
+    ArrayList<String> list = sqlHandler.selectDate(1666262249300L, 1666262249400L);
+    Gson gson = new Gson();
+    String jsonArray = gson.toJson(list);
+    return jsonArray;
   }
 }
