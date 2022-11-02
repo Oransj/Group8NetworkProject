8c882050adef6cebc31bffeeee72b774f96cdcb3
z-rayc <zilerayser@gmail.com>
2022-11-02 20:10:41 +0100

    Create functions to get summary, average values and weather type

--- project/src/main/java/no/ntnu/idata2304/group8/weather/WeatherSummary.java
+++ project/src/main/java/no/ntnu/idata2304/group8/weather/WeatherSummary.java
@@ -1,12 +1,11 @@
 package no.ntnu.idata2304.group8.weather;
 
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
 
 /**
- * Gives a simplified summary of the weather based on factors like
+ * Get a simplified summary of the weather based on factors like
  * time, temperature, precipitation, atmospheric pressure, and wind.
  *
  * Sources
@@ -16,8 +15,110 @@ import java.util.List;
  * Thunder:     https://www.weather.gov/source/zhu/ZHU_Training_Page/thunderstorm_stuff/Thunderstorms/thunderstorms.htm
  */
 public class WeatherSummary {
-    public String convertSummaryToText(List<Boolean> summary) {
-        return null;
+    /**
+     * Get the summary for a day.
+     *
+     * @param dayData A list of weather data rows for the entire day.
+     *                Each row in the format {temperature, precipitation, pressure, light, wind}.
+     * @return  A list of minimum temperature, maximum temperature,
+     *          average precipitation and average wind strength.
+     */
+    public Float[] getDaySummary(List<Float[]> dayData) {
+        float minTemp = dayData.get(0)[0];
+        float maxTemp = dayData.get(0)[0];
+        float totalPrecip = 0;
+        float totalWind = 0;
+        float avgPrecip;
+        float avgWind;
+    
+        for (Float[] row : dayData) {
+            if (row[0] < minTemp) {
+                minTemp = row[0];
+            } else if (row[0] > maxTemp) {
+                maxTemp = row[0];
+            }
+            
+            totalPrecip += row[1];
+            totalWind += row[4];
+        }
+        
+        avgPrecip = totalPrecip / dayData.size();
+        avgWind = totalWind / dayData.size();
+        
+        return new Float[]{minTemp, maxTemp, avgPrecip, avgWind};
+    }
+    
+    /**
+     * Get list of average values from a list of rows with values.
+     *
+     * @param dataRows A list of weather data rows for a time interval.
+     *                 Each row is in the format {temperature,
+     *                 precipitation, pressure, light, wind}.
+     * @return  An array of float values in the format
+     *          {temperature, precipitation, pressure, light, wind}.
+     */
+    public Float[] getAverageValuesInInterval(List<Float[]> dataRows) {
+        float totalTemp = 0;
+        float totalPrecip = 0;
+        float totalPressure = 0;
+        float totalLight = 0;
+        float totalWind = 0;
+        int size = dataRows.size();
+        
+        for (Float[] row : dataRows) {
+            totalTemp += row[0];
+            totalPrecip += row[1];
+            totalPressure += row[2];
+            totalLight += row[3];
+            totalWind += row[4];
+        }
+        
+        return new Float[]{totalTemp/size, totalPrecip/size,
+            totalPressure/size, totalLight/size, totalWind/size};
+    }
+    
+    /**
+     * Take numerical values representing weather types and convert it to a single text value.
+     *
+     * @param summary Weather values that correspond to a type of weather.
+     *                {sunOrMoon, rainOrSnow, thunder}.
+     * @return The type of weather.
+     */
+    public String getWeatherType(List<Integer> summary) {
+        // The values in the summary list
+        int sunOrMoon = summary.get(0);
+        int rainOrSnow = summary.get(1);
+        int thunder = summary.get(2);
+        
+        String weatherAsString;
+        
+        // Based on the summary list values, find the corresponding weather types
+        if (thunder == 1) { // Thunder
+            weatherAsString = "thunder";
+        } else if (rainOrSnow == 0) { // No precipitation
+            weatherAsString = switch (sunOrMoon) {
+                case -2 -> "cloud-with-moon";
+                case -1 -> "moon";
+                case 0 -> "cloud";
+                case 1 -> "cloud-with-sun";
+                case 2 -> "sun";
+                default -> "";
+            };
+        } else if (rainOrSnow < 0) { // Snow
+             if (sunOrMoon == 1 || sunOrMoon == 2) {
+                 weatherAsString = "snow-with-sun";
+             } else {
+                 weatherAsString = "snow";
+             }
+        } else { // Rain
+            if (sunOrMoon == 1 || sunOrMoon == 2) {
+                weatherAsString = "rain-with-sun";
+            } else {
+                weatherAsString = "rain";
+            }
+        }
+        
+        return weatherAsString;
     }
   
     /**
@@ -28,20 +129,20 @@ public class WeatherSummary {
      * @return List of the weather summary in the format
      * '{sunOrMoon, rainOrSnow, thunder}', where each variable has numerical values.
      */
-    public List<Integer> getWeatherSummary(String[] data) {
+    public List<Integer> getWeatherSummary(Float[] data) {
         List<Integer> weatherSummary = new ArrayList<>();
         
         // The items in the data array
-        int temp = Integer.parseInt(data[0]);
-        int precipitation = Integer.parseInt(data[1]);
-        int pressure = Integer.parseInt(data[2]);
-        int light = Integer.parseInt(data[3]);
-        int wind = Integer.parseInt(data[4]);
+        float temp = data[0];
+        float precipitation = data[1];
+        float pressure = data[2];
+        float light = data[3];
+        float wind = data[4];
         
         // The summaries
         int sunOrMoon = getSunSummary(light);
         int rainOrSnow = getPrecipitationSummary(temp, precipitation);
-        int thunder = getThunderSummary(temp, rainOrSnow, pressure, wind);
+        int thunder = getThunderSummary(temp, rainOrSnow, wind);
 
         // Add the info to the map
         weatherSummary.add(sunOrMoon);
@@ -57,7 +158,7 @@ public class WeatherSummary {
      * @param light The amount of light (lux).
      * @return -2 for cloudy night, -1 for moonlight, 0 for clouds, 1 for clouds and sun, 2 for sun.
      */
-    private int getSunSummary(int light) {
+    private int getSunSummary(float light) {
         int sun;
         if (light < 1) {
             sun = -2; // Cloudy night
@@ -80,7 +181,7 @@ public class WeatherSummary {
      * @param precipitation The amount of precipitation (millimeter).
      * @return -2 for much snow, -1 for snow, 0 for no precipitation, 1 for rain, 2 for much rain.
      */
-    private int getPrecipitationSummary(int temperature, int precipitation) {
+    private int getPrecipitationSummary(float temperature, float precipitation) {
         int rain;
         if (precipitation < 0.2) {
             rain = 0;
@@ -105,11 +206,10 @@ public class WeatherSummary {
      *
      * @param temperature The temperature (celsius).
      * @param precipitationLevel The precipitation level (-1, 0, 1).
-     * @param pressure The atmospheric pressure (hPa).
      * @param wind The wind strength (m/s).
      * @return 0 for no thunder, 1 for thunder.
      */
-    private int getThunderSummary(int temperature, int precipitationLevel, int pressure, int wind) {
+    private int getThunderSummary(float temperature, int precipitationLevel, float wind) {
         int thunder = 0;
         if ((temperature > 13 && precipitationLevel == 1)
         || (precipitationLevel == 2 && wind > 10)) {
