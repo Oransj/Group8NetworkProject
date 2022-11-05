33def06cd3793155cefe6be28ce544964b6650b5
Mark Kour <mark.kour@icloud.com>
2022-11-05 17:00:48 +0100

    Created WeatherSorting class with a test method printJSON

---
+++ project/src/main/java/no/ntnu/idata2304/group8/webserver/WeatherSorting.java
@@ -0,0 +1,10 @@
+package no.ntnu.idata2304.group8.webserver;
+
+import org.json.simple.JSONObject;
+
+public class WeatherSorting {
+
+    public void printJSON(JSONObject jsonObject) {
+        System.out.println(jsonObject);
+    }
+}
