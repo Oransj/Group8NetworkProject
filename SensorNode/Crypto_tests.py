62f314008777d18eafb8464b03c4f1d161eaecb6
Oransj <chrion@stud.ntnu.no>
2022-11-07 20:11:34 +0100

    Update Crypto_tests.py

--- SensorNode/Crypto_tests.py
+++ SensorNode/Crypto_tests.py
@@ -1,6 +1,7 @@
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
+from cryptography.hazmat.primitives import serialization
 import timeit
 
 start = timeit.default_timer()
@@ -17,14 +18,11 @@ private_key = key.private_bytes(
     crypto_serialization.NoEncryption()
 )
 
-public_key = key.public_key().public_bytes(
-    crypto_serialization.Encoding.OpenSSH,
-    crypto_serialization.PublicFormat.OpenSSH
-)
+public_key = key.public_key()
 
 stop = timeit.default_timer()
 
 print(private_key.decode('utf-8'))
-print(public_key.decode('utf-8'))
+print(public_key)
 print("-------------------")
 print("Time: ", stop - start)
