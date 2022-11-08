5b35974a7d2c30bb4ce954a897035204cefd3af6
Mark Kour <mark.kour@icloud.com>
2022-11-08 11:00:33 +0100

    Moved the listener to main

--- project/src/main/java/no/ntnu/idata2304/group8/webserver/MainServer.java
+++ project/src/main/java/no/ntnu/idata2304/group8/webserver/MainServer.java
@@ -1,5 +1,6 @@
 package no.ntnu.idata2304.group8.webserver;
 
+import no.ntnu.idata2304.group8.MQTT.MQTTListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,8 @@ public class MainServer {
 
   public static void main(String[] args) {
     SpringApplication.run(MainServer.class, args);
+    MQTTListener listener = new MQTTListener("admin");
+    Thread thread = new Thread(listener);
+    thread.start();
   }
 }
