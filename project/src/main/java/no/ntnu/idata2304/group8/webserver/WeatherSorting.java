a1ed6358d8bee83285aae1a59a279f9d2da577ec
Mark Kour <mark.kour@icloud.com>
2022-11-12 09:31:41 +0100

    Created getHomePage method

--- project/src/main/java/no/ntnu/idata2304/group8/webserver/WeatherSorting.java
+++ project/src/main/java/no/ntnu/idata2304/group8/webserver/WeatherSorting.java
@@ -1,10 +1,34 @@
 package no.ntnu.idata2304.group8.webserver;
 
-import org.json.simple.JSONObject;
+import no.ntnu.idata2304.group8.databasehandler.SQLHandler;
+import no.ntnu.idata2304.group8.weather.WeatherSummary;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WeatherSorting {
+    SQLHandler sqlHandler = new SQLHandler();
+    WeatherSummary weatherSummary = new WeatherSummary();
+    public ArrayList<Double[]> getHomePage(String[] days) {
+        // TODO: get weather type
+        List<Double[]> day1Data = sqlHandler.selectWeatherDataBetween(Long.parseLong(days[0]), Long.parseLong(days[1]), "weather");
+        Double[] day1AvgData = weatherSummary.getDaySummary(day1Data);
 
-    public void printJSON(JSONObject jsonObject) {
-        System.out.println(jsonObject);
+        List<Double[]> day2Data = sqlHandler.selectWeatherDataBetween(Long.parseLong(days[1]), Long.parseLong(days[2]), "weather");
+        Double[] day2AvgData = weatherSummary.getDaySummary(day2Data);
+
+        List<Double[]> day3Data = sqlHandler.selectWeatherDataBetween(Long.parseLong(days[2]), Long.parseLong(days[3]), "weather");
+        Double[] day3AvgData = weatherSummary.getDaySummary(day3Data);
+
+        List<Double[]> day4Data = sqlHandler.selectWeatherDataBetween(Long.parseLong(days[3]), Long.parseLong(days[4]), "weather");
+        Double[] day4AvgData = weatherSummary.getDaySummary(day4Data);
+
+        ArrayList<Double[]> avgDaysData = new ArrayList<>();
+        avgDaysData.add(day1AvgData);
+        avgDaysData.add(day2AvgData);
+        avgDaysData.add(day3AvgData);
+        avgDaysData.add(day4AvgData);
+
+        return avgDaysData;
     }
 }
