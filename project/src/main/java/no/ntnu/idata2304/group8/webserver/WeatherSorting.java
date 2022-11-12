ec4a3b786a65ee6bb4bf65c7738fc29b13abe852
Mark Kour <mark.kour@icloud.com>
2022-11-12 23:16:35 +0100

    Created getWeatherType method

--- project/src/main/java/no/ntnu/idata2304/group8/webserver/WeatherSorting.java
+++ project/src/main/java/no/ntnu/idata2304/group8/webserver/WeatherSorting.java
@@ -31,4 +31,24 @@ public class WeatherSorting {
 
         return avgDaysData;
     }
+
+    public String[] getWeatherType(String[] days) {
+        List<Double[]> day1Data = sqlHandler.selectWeatherDataBetween(Long.parseLong(days[0]), Long.parseLong(days[1]), "weather");
+        Double[] day1AvgData = weatherSummary.getAverageValuesInInterval(day1Data);
+        String day1Type = weatherSummary.getWeatherSummary(day1AvgData);
+
+        List<Double[]> day2Data = sqlHandler.selectWeatherDataBetween(Long.parseLong(days[1]), Long.parseLong(days[2]), "weather");
+        Double[] day2AvgData = weatherSummary.getAverageValuesInInterval(day1Data);
+        String day2Type = weatherSummary.getWeatherSummary(day2AvgData);
+
+        List<Double[]> day3Data = sqlHandler.selectWeatherDataBetween(Long.parseLong(days[2]), Long.parseLong(days[3]), "weather");
+        Double[] day3AvgData = weatherSummary.getAverageValuesInInterval(day1Data);
+        String day3Type = weatherSummary.getWeatherSummary(day3AvgData);
+
+        List<Double[]> day4Data = sqlHandler.selectWeatherDataBetween(Long.parseLong(days[3]), Long.parseLong(days[4]), "weather");
+        Double[] day4AvgData = weatherSummary.getAverageValuesInInterval(day1Data);
+        String day4Type = weatherSummary.getWeatherSummary(day4AvgData);
+
+        return new String[]{day1Type, day2Type, day3Type, day4Type};
+    }
 }
