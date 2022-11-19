a48258edc2c463fdca7bd6f40c2d7cf0d6ec849a
Oransj <chrion@stud.ntnu.no>
2022-11-19 18:59:44 +0100

    Fix bug where time not measured properly

--- SensorNode/WeatherSimulation.py
+++ SensorNode/WeatherSimulation.py
@@ -540,7 +540,7 @@ def main():
             else:
                 storm = False
             percipitation_today = percipitation_sim.generate_percipitation_today(percipitation_today)
-        if(next_time <= now.minute):
+        if(next_time <= (now.minute + now.hour * 60)):
             print("Creating new data")
             time = now.time()
             percipitation_now = percipitation_today[now.hour]  * (weights().minutes_update/60)
@@ -559,7 +559,7 @@ def main():
             print("Data published")
             next_time = datetime.datetime.now() + datetime.timedelta(minutes=weights().minutes_update)
             print(f"Next update at {next_time}")
-            next_time = next_time.time().minute
+            next_time = next_time.time().minute + next_time.time().hour*60
         print("sleeping " + str(now))
         t.sleep(weights().sleep_time)
         now = datetime.datetime.now()
